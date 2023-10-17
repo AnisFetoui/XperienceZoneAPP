0ebd72199d2882bb32973a703d3599d63d2d6e8f
imen hadj yahya <115800805+ImenHadj@users.noreply.github.com>
2023-10-17 16:18:48 +0100

    Add files via upload

--- pidev hooligans/src/view/GestionCategorieController.java
+++ pidev hooligans/src/view/GestionCategorieController.java
@@ -8,9 +8,8 @@ package view;
 import classes.Categorie;
 import java.io.IOException;
 import java.net.URL;
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -25,12 +24,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.SortEvent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import service.CategorieService;
-import util.MYDB;
 
 /**
  * FXML Controller class
@@ -45,9 +44,10 @@ public class GestionCategorieController implements Initializable {
     private Button ajouter;
     @FXML
     private Button modifier;
-      public static int id_modif ;  
     @FXML
     private Button supprimer;
+    @FXML
+    private TableView<Categorie> tableview;
     @FXML
     private TableColumn<Categorie, Integer> id_categ;
     @FXML
@@ -56,76 +56,101 @@ public class GestionCategorieController implements Initializable {
     private TableColumn<Categorie, String> des_col;
     @FXML
     private TableColumn<Categorie, String> type_col;
-@FXML
- private TableView<Categorie> tableview;
+ public static int id_modif ;
     /**
      * Initializes the controller class.
-     * @throws java.sql.SQLException
      */
-     @FXML
- public void afficher() throws SQLException
-    {
-         CategorieService c = new CategorieService();
-
-        ArrayList<Categorie> lc  = c.afficher();
-        ObservableList<Categorie> categorieArrayList = FXCollections.observableArrayList(lc);
-        id_categ.setCellValueFactory( new PropertyValueFactory<>("Id_categorie"));
-         nom_categ.setCellValueFactory(new PropertyValueFactory<>("nom_categorie"));
-            des_col.setCellValueFactory(new PropertyValueFactory<>("description_categorie"));
-            type_col.setCellValueFactory(new PropertyValueFactory<>("type_categorie"));
-           
-         tableview.setItems(categorieArrayList);
-       
-    }
- 
- public  GestionCategorieController(){ 
-    Connection con = MYDB.getinstance().getCon();
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        try {
-            afficher();  // TODO
-        } catch (SQLException ex) {
-            Logger.getLogger(GestionCategorieController.class.getName()).log(Level.SEVERE, null, ex);
-        
-
-        }
-    }
-
-
-       
    
 
-@FXML
-private void modifierCategorie(ActionEvent event) {
-Categorie selectedCategorie = tableview.getSelectionModel().getSelectedItem();
+    @FXML
+    private void afficher() {
+        try {
+            CategorieService c = new CategorieService();
+            
+            List<Categorie> lu = c.afficher();
+            ObservableList<Categorie> categorieList = FXCollections.observableArrayList(lu);
+            id_categ.setCellValueFactory(new PropertyValueFactory<>("id_categorie"));
+            nom_categ.setCellValueFactory(new PropertyValueFactory<>("nom_categorie"));
+            des_col.setCellValueFactory(new PropertyValueFactory<>("description_categorie"));
+            type_col.setCellValueFactory(new PropertyValueFactory<>("type_categorie"));
+            
+            tableview.setItems(categorieList);
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionCategorieController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+     @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       
+            // TODO
+            afficher();
+        
+    }   
+    @FXML
+    private void afficherAction(ActionEvent event) throws SQLException {
+       afficher();
+    }
+    
 
-    if (selectedCategorie != null) {
+    @FXML
+    private void ajouterCategorie(ActionEvent event) {
+        try {
+
+            Parent page1 = FXMLLoader.load(getClass().getResource("Ajoutercategorie.fxml"));
+
+            Scene scene = new Scene(page1);
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            stage.setScene(scene);
+
+            stage.show();
+
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+
+        }
+        
+    
+    }
+
+    @FXML
+
+private void modifierCategorie(ActionEvent event) {
+    int SelectedRowIndex = tableview.getSelectionModel().getSelectedIndex();
+    int ColumnIndex = tableview.getColumns().indexOf(id_categ);
+    
+    Alert A = new Alert(Alert.AlertType.CONFIRMATION);
+    if (SelectedRowIndex == -1) {
+        A.setAlertType(Alert.AlertType.WARNING);
+        A.setContentText("Selectionnez une colonne ! ");
+        A.show();
+    } else {
+        String IdCell = tableview.getColumns().get(ColumnIndex).getCellData(SelectedRowIndex).toString();
+        id_modif = Integer.parseInt(IdCell);
+        
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("Modifiercategorie.fxml"));
             Parent root = loader.load();
             ModifiercategorieController modifierController = loader.getController();
-            modifierController.initializeData(selectedCategorie); // Passez la catégorie sélectionnée au contrôleur de modification
+            
+            // Obtenez la catégorie sélectionnée à partir de votre TableView
+            Categorie selectedCategorie = tableview.getSelectionModel().getSelectedItem();
+            
+            // Transmettez la catégorie sélectionnée au contrôleur de modification
+            modifierController.initializeData(selectedCategorie);
+            
+            // Ajoutez des messages de débogage pour vérifier la catégorie sélectionnée
+            System.out.println("Selected category: " + selectedCategorie);
+            
             Scene scene = new Scene(root);
-            Stage stage = new Stage();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(scene);
             stage.show();
         } catch (IOException ex) {
-            ex.printStackTrace();
+            System.out.println(ex.getMessage());
         }
-    } else {
-        showErrorAlert("Aucune catégorie sélectionnée", "Veuillez sélectionner une catégorie à modifier.");
     }
-}
-
-private void showErrorAlert(String title, String content) {
-    Alert alert = new Alert(Alert.AlertType.WARNING);
-    alert.setTitle(title);
-    alert.setHeaderText(null);
-    alert.setContentText(content);
-    alert.showAndWait();
-}
-}
-
+}}
 
--- pidev hooligans/src/view/ModifiercategorieController.java
+++ pidev hooligans/src/view/ModifiercategorieController.java
@@ -12,10 +12,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
+import javafx.scene.Node;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import service.CategorieService;
 
 /**
@@ -23,176 +23,66 @@ import service.CategorieService;
  *
  * @author ASUS
  */
-/*public class ModifiercategorieController implements Initializable {
- CategorieService categorieservice;
-        Categorie c ;
+public class ModifiercategorieController implements Initializable {
+
     @FXML
     private TextField modifnom;
     @FXML
     private TextField modifdesc;
     @FXML
+    
     private ChoiceBox<String> modifierchoix;
-     private String[] choix ={"sport","Loisirs"} ;
+private Categorie selectedCategorie;
 
-    public ModifiercategorieController() throws SQLException {
-        this.categorieservice = new CategorieService();
+    public void initializeData(Categorie categorie) {
+    System.out.println("initializeData called with category: " + categorie);
+    selectedCategorie = categorie;
+    if (categorie != null) {
+        // Remplissez les champs de formulaire ici
+        modifnom.setText(categorie.getNom_categorie());
+        modifdesc.setText(categorie.getDescription_categorie());
+        modifierchoix.setValue(categorie.getType_categorie());
     }
+}
 
+
+    
+        
     /**
      * Initializes the controller class.
      */
-    /*@Override
-    public void initialize(URL url, ResourceBundle rb) {
-       modifierchoix.getItems().addAll(choix);
-        
-    }    
-    
-    void getCategorie(Categorie c){
-    modifnom.setText(c.getNom_categorie());
-    modifdesc.setText(c.getDescription_categorie());
-    modifierchoix.setValue(c.getType_categorie());
-}
-@FXML
-    private void Modifiercat(ActionEvent event) {
-        
-        if (modifnom.getText().length() == 0||modifdesc.getText().length() == 0||modifierchoix.getValue() == null) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Erreur");
-            alert.setHeaderText("Erreur de saisie !");
-            alert.setContentText("Veuillez remplir tous les champs"+ "");
-            alert.show();
-
-        
-              } else if(modifnom.getText().matches("\\d*")){
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Erreur");
-            alert.setHeaderText("Erreur de saisie !");
-            alert.setContentText("Le nom de catégorie doit etre une chaine"+ "");
-            alert.show(); 
-    }
-        else {
-         try {
-            c.setNom_categorie(modifnom.getText());
-            c.setDescription_categorie(modifdesc.getText());
-            c.setType_categorie(modifierchoix.getValue());
-            
-            
-            categorieservice.modifier(c);
-           
-        } catch (Exception ex) {
-             System.out.println(ex);        }
-        
-    }}
-    
-}*/
-public class ModifiercategorieController implements Initializable {
-    CategorieService categorieservice;
-    Categorie c;
-
-    @FXML
-    private TextField modifnom;
-    @FXML
-    private TextField modifdesc;
-    @FXML
-    private ChoiceBox<String> modifierchoix;
-    private String[] choix = {"sport", "Loisirs","activité"};
-
-    public ModifiercategorieController() {
-        try {
-            this.categorieservice = new CategorieService();
-            this.c = new Categorie(); // Initialize the Categorie object
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        modifierchoix.getItems().addAll(choix);
-    }
-
-    void getCategorie(Categorie c) {
-        modifnom.setText(c.getNom_categorie());
-        modifdesc.setText(c.getDescription_categorie());
-        modifierchoix.setValue(c.getType_categorie());
-    }
-
-  
-   // private void Modifiercat(ActionEvent event) {
-        /*if (modifnom.getText().isEmpty() || modifdesc.getText().isEmpty() || modifierchoix.getValue() == null) {
-            showErrorAlert("Erreur de saisie !", "Veuillez remplir tous les champs.");
-        } else if (modifnom.getText().matches("\\d*")) {
-            showErrorAlert("Erreur de saisie !", "Le nom de catégorie doit être une chaîne.");
-        } else {
-            //c.setId_categorie(6);
-            c.setNom_categorie(modifnom.getText());
-            c.setDescription_categorie(modifdesc.getText());
-            c.setType_categorie(modifierchoix.getValue());
-
-            try {
-                categorieservice.modifier(c);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-    }*/
-       public void initializeData(Categorie categorie) {
-        if (categorie != null) {
-            modifnom.setText(categorie.getNom_categorie());
-            modifdesc.setText(categorie.getDescription_categorie());
-            modifierchoix.setValue(categorie.getType_categorie());
-        }
-    }
+        // TODO
+         modifierchoix.getItems().addAll("Choix 1", "Choix 2", "Choix 3");
+    }    
 
     @FXML
-    
-private void Modifiercat(ActionEvent event) {
-Categorie categorie = null;
-if (categorie != null) {
-    if (modifnom.getText().isEmpty() || modifdesc.getText().isEmpty() || modifierchoix.getValue() == null) {
-        showErrorAlert("Erreur de saisie !", "Veuillez remplir tous les champs.");
-    } else if (modifnom.getText().matches("\\d*")) {
-        showErrorAlert("Erreur de saisie !", "Le nom de catégorie doit être une chaîne.");
-    } else {
-        // Mettez à jour les propriétés de la catégorie avec les valeurs des champs de formulaire
-        
-        categorie.setNom_categorie(modifnom.getText());
-        categorie.setDescription_categorie(modifdesc.getText());
-        categorie.setType_categorie(modifierchoix.getValue());
+    private void Modifiercat(ActionEvent event) throws SQLException  {
+        if (selectedCategorie != null) {
+        // Récupérez les valeurs modifiées à partir des champs de formulaire
+        String nouveauNom = modifnom.getText();
+        String nouvelleDescription = modifdesc.getText();
+        String nouveauType = modifierchoix.getValue();
 
-        try {
-            // Utilisez le service déjà existant
-            CategorieService categorieService = new CategorieService();
-            categorieService.modifier(categorie);
+        // Mettez à jour les propriétés de selectedCategorie
+        selectedCategorie.setNom_categorie(nouveauNom);
+        selectedCategorie.setDescription_categorie(nouvelleDescription);
+        selectedCategorie.setType_categorie(nouveauType);
 
-            // Fermez la fenêtre de modification après la mise à jour
-            closeStage();
-        } catch (SQLException ex) {
-            // Gérez l'exception correctement, par exemple, affichez un message d'erreur
-            showErrorAlert("Erreur de modification", "Une erreur s'est produite lors de la modification de la catégorie.");
-        }
-    }
-} else {
-    // Si categorie est null, affichez un message d'erreur
-    showErrorAlert("Erreur", "Aucune catégorie sélectionnée pour la modification.");
-}
-}
+        // Appelez votre service pour mettre à jour la catégorie dans la base de données
+        CategorieService categorieService = new CategorieService();
+        categorieService.modifier(selectedCategorie); // Gérez l'exception correctement, par exemple, affichez un message d'erreur
 
-
-    private void showErrorAlert(String title, String content) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle(title);
-        alert.setHeaderText("Erreur");
-        alert.setContentText(content);
-        alert.showAndWait();
-    }
-
-    void setBorderPane(BorderPane borderPane) {
-      
-    }
-
-   
-    private void closeStage() {
-       
+        // Fermez la fenêtre de modification
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
     }
 }
+
+
+
+
+
+    }
+
